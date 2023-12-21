105a45e74a6bb5532406a620caf726d26ada709b
Sasha <sashakozoriz2012@gmail.com>
2023-12-21 02:20:26 +0200

    Fix navigation links and add menu functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,3 +12,12 @@ uaButton.addEventListener('click', () => {
   enButton.classList.add('nav__link__lang--not-active');
   uaButton.classList.remove('nav__link__lang--not-active');
 });
+
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
